5c0a967b7815241bec1e6cc3c6e37d0c8dae111e
Mert Gökmen <mertgkmeen@gmail.com>
2021-12-28 22:04:46 +0300

    two more

--- python_problems_2.py
+++ python_problems_2.py
@@ -21,14 +21,14 @@
 
 
 if __name__ == '__main__':
-    n = int(raw_input())
+    n = int(input())
     student_marks = {}
     for _ in range(n):
-        line = raw_input().split()
+        line = input().split()
         name, scores = line[0], line[1:]
         scores = map(float, scores)
         student_marks[name] = scores
-    query_name = raw_input()
+    query_name = input()
 
     total = 0
     for i in student_marks[query_name]:
@@ -36,3 +36,64 @@ if __name__ == '__main__':
     res = total/3
     formatted = "{:.2f}".format(res)
     print(formatted)
+
+# Lists
+
+# https://www.hackerrank.com/challenges/python-lists/
+
+# Consider a list(list=[]). You can perform the following commands:
+
+# insert i e: Insert integer  at position .
+# print: Print the list.
+# remove e: Delete the first occurrence of integer .
+# append e: Insert integer  at the end of the list.
+# sort: Sort the list.
+# pop: Pop the last element from the list.
+# reverse: Reverse the list.
+# Initialize your list and read in the value of  followed by  lines of
+# commands where each command will be of the  types
+# listed above. Iterate through each command in order and perform the
+# corresponding operation on your list.
+
+    list = []
+N = int(input())
+for line in range(N):
+    cmd = input().split()
+    if cmd[0] == "insert":
+        list.insert(int(cmd[1]), int(cmd[2]))
+    elif cmd[0] == "print":
+        print(list)
+    elif cmd[0] == "remove":
+        list.remove(int(cmd[1]))
+    elif cmd[0] == "sort":
+        list.sort()
+    elif cmd[0] == "pop":
+        list.pop()
+    elif cmd[0] == "reverse":
+        list.reverse()
+    elif cmd[0] == "append":
+        list.append(int(cmd[1]))
+
+#Tuples
+
+#https://www.hackerrank.com/challenges/python-tuples/
+
+# Task
+# Given an integer,n , and n space-separated integers as input, create a tuple,t , of those n integers.
+#  Then compute and print the result of hash(t).
+
+# Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
+
+# Input Format
+
+# The first line contains an integer, , denoting the number of elements in the tuple.
+# The second line contains  space-separated integers describing the elements in tuple .
+
+# Output Format
+
+# Print the result of hash(t).
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = tuple(map(int, input().split()))
+    print(hash(integer_list))
