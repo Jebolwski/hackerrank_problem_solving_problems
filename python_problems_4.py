c4139d1a4d7477b5953f09ec7c00d9e5f6669803
Mert Gökmen <mertgkmeen@gmail.com>
2022-01-01 12:36:41 +0300

    three more

--- python_problems_4.py
+++ python_problems_4.py
@@ -14,6 +14,46 @@
 # #####
 # ######
 
+# Mini-Max Sum
+
+# https: // www.hackerrank.com/challenges/mini-max-sum
+
+# Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+# Example
+
+# The minimum sum is and the maximum sum is . The function prints
+
+# 16 24
+# Function Description
+
+# Complete the miniMaxSum function in the editor below.
+
+# miniMaxSum has the following parameter(s):
+
+# arr: an array of  integers
+# Print
+
+# Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+# Input Format
+
+# A single line of five space-separated integers.
+
+# Constraints
+
+
+# Output Format
+
+# Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+# Sample Input
+
+# 1 2 3 4 5
+# Sample Output
+
+# 10 14
+
 
 def staircase(n):
     for i in range(n):
@@ -22,3 +62,10 @@ def staircase(n):
         for k in range(0, i+1):
             print("#", end="")
         print("")
+
+
+        def miniMaxSum(arr):
+    arr.sort()
+    min = arr[0]+arr[1]+arr[2]+arr[3]
+    max = arr[1]+arr[2]+arr[3]+arr[4]
+    print(min, max)
---
+++ python_problems_5.py
@@ -0,0 +1,49 @@
+# Time Conversion
+
+# https: // www.hackerrank.com/challenges/time-conversion
+
+# Given a time in -hour AM/PM format, convert it to military(24-hour) time.
+
+# Sample Input 0
+
+# 07: 05: 45PM
+# Sample Output 0
+
+# 19: 05: 45
+
+def timeConversion(s):
+    array = []
+    for i in s:
+        array.append(i)
+    if array[-2] == "P":
+        if s[:2] == "12":
+            return s[0:8]
+        else:
+            a = str(int(s[:2])+12)
+            return a+s[2:8]
+
+    else:
+        if s[:2] == "12":
+            return "00"+s[2:8]
+        else:
+            return s[:8]
+
+
+# Mini-Max Sum
+
+# https: // www.hackerrank.com/challenges/mini-max-sum
+
+# Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+# Example
+
+# The minimum sum is and the maximum sum is . The function prints
+
+# 16 24
+
+
+def miniMaxSum(arr):
+    arr.sort()
+    min = arr[0]+arr[1]+arr[2]+arr[3]
+    max = arr[1]+arr[2]+arr[3]+arr[4]
+    print(min, max)
