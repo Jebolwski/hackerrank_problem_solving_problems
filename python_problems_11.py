d1f19403304ad2446bcaac4438072df9e276346d
Mert Gökmen <mertgkmeen@gmail.com>
2022-01-12 12:17:55 +0300

    two more

--- python_problems_11.py
+++ python_problems_11.py
@@ -20,3 +20,24 @@ def hurdleRace(k, height):
     #             count=count+1
     #             if i==k:
     #                 return count
+
+
+# https://www.hackerrank.com/challenges/designer-pdf-viewer
+
+    def designerPdfViewer(h, word):
+        alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        array = []
+        for i in word:
+            array.append(h[alphabet.index(i)])
+        return max(array)*len(word)    
+
+# https://www.hackerrank.com/challenges/utopian-tree
+
+    def utopianTree(n):
+        height=1
+        for i in range(1,n+1):
+            if i%2==0:
+                height=height+1
+            else:
+                height=height*2 
+        return height
