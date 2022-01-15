43ceb321e3cb44c07dbebee1d2ca830dae63eebf
Mert Gökmen <mertgkmeen@gmail.com>
2022-01-15 19:39:24 +0300

    4 stars

--- python_problems_12.py
+++ python_problems_12.py
@@ -15,3 +15,33 @@ def permutationEquation(p):
     for i in range(1,len(p)+1):
         result.append(p.index(p.index(i)+1)+1)
     return result
+
+# https://www.hackerrank.com/challenges/non-divisible-subset
+
+def nonDivisibleSubset(k, s):
+    # array = []
+    # for i in range(len(s)):
+    #     for j in range(i+1,len(s)):
+    #         print("j :",j,"i :",i)
+    #         array.append(array[i]+array[j])
+    # array = list(set(array))
+    # for i in array:
+    #     if i%k==0 or k%i==0:
+    #         array.remove(i)
+    # return len(array)
+    remainder = [0] * k
+    for i in s:
+        remainder[i%k]+=1
+    
+    maxnum = 0
+    maxnum += min(remainder[0],1)
+    
+    if k%2==0:
+        maxnum+=min(remainder[k//2],1)
+        
+    for i in range(1,k//2+1):
+        if i!=k-i:
+            maxnum+=max(remainder[i],remainder[k-i])
+    
+            
+    return maxnum
