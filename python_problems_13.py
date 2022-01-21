a9d1de83cd2d690be886531206319e035372f098
Mert Gökmen <mertgkmeen@gmail.com>
2022-01-21 10:44:40 +0300

    two more

--- python_problems_13.py
+++ python_problems_13.py
@@ -15,4 +15,34 @@ def encryption(s):
         result.append("".join(temp))
         
     return " ".join(result)
+
+# https://www.hackerrank.com/challenges/beautiful-triplets
+
+def beautifulTriplets(d, arr):
+    count=0
+    m=Counter(arr)
+    for num in arr:
+        if m[num+d] and m[num+d+d]:
+            count=count+1
+    return count
+
+# https://www.hackerrank.com/challenges/minimum-distances
+
+def minimumDistances(a):
+    array=[]
+    array1=[]
+    for i in range(len(a)):
+        mini=a[i]
+        for j in range(len(a)):
+            if a[j]==a[i]:
+                array.append(abs(i-j))
+    array.sort()
+    for item in array:
+        if item not in array1:
+            array1.append(item)
+    if len(array1)==1:
+        return "-1"
+    else:
+        return array1[1]
+
         
