e36b5eb6d5f52fd789f2b1292bc1a053d92b99dd
Mert Gökmen <mertgkmeen@gmail.com>
2022-02-13 12:46:35 +0300

    one more

--- python_problems_15.py
+++ python_problems_15.py
@@ -223,3 +223,14 @@ def theLoveLetterMystery(s):
         
     return count
                 
+# https://www.hackerrank.com/challenges/insertion-sort/
+
+def insertionSort(arr):
+    count=0
+    for i in range(len(arr)):
+        for j in range(i):
+            if arr[j]>arr[i]:
+                arr[i],arr[j]==arr[j],arr[i]
+                count=count+1
+    return count
+        
