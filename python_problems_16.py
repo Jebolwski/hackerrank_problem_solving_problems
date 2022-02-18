50935a6ed0dd7b31c112fdc003c7838caf50438b
Mert Gökmen <mertgkmeen@gmail.com>
2022-02-18 11:41:13 +0300

    three more

--- python_problems_16.py
+++ python_problems_16.py
@@ -1,3 +1,5 @@
+import math
+
 # https://www.hackerrank.com/challenges/string-construction/
 
 def stringConstruction(s):
@@ -33,3 +35,47 @@ def isValid(s):
         if arr2.count(i)==1:
             return "YES"
     return "NO"
+
+# https://www.hackerrank.com/challenges/priyanka-and-toys
+
+def toys(w):
+    w.sort()
+    temp = w[0]
+    count = 1
+    for i in w:
+        if i > temp + 4:
+            temp = i
+            count += 1
+    return count
+
+# https://www.hackerrank.com/challenges/strings-xor
+
+def strings_xor(s, t):
+    res = ""
+    for i in range(len(s)):
+        if s[i] == t[i]:
+            res += '0'
+        else:
+            res += '1'
+
+    return res
+
+s = input()
+t = input()
+print(strings_xor(s, t))
+
+# https://www.hackerrank.com/challenges/smart-number
+
+def is_smart_number(num):
+    val = int(math.sqrt(num))
+    if num / (val*val) == 1:
+        return True
+    return False
+
+for _ in range(int(input())):
+    num = int(input())
+    ans = is_smart_number(num)
+    if ans:
+        print("YES")
+    else:
+        print("NO")
