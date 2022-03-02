5c07738a545c9d522a13269d0b1ca0653bbd8271
Mert Gökmen <mertgkmeen@gmail.com>
2022-03-02 15:42:35 +0300

    one more

--- python_problems_17.py
+++ python_problems_17.py
@@ -9,17 +9,17 @@ words_lite=[]
 for i in range(n):
     word = input().strip()
     words.append(word)
-    
-    
+
+
 for i in words:
     if i not in words_lite:
         words_lite.append(i)
-        
-        
+
+
 for i in words_lite:
-    count.append(words.count(i))  
-    
-    
+    count.append(words.count(i))
+
+
 print(len(words_lite))
 print(*count)
 
@@ -44,14 +44,34 @@ for i in range(N):
 for i in names:
     if i not in names_lite:
         names_lite.append(i)
-        
+
 for i in prices:
     if i not in prices_lite:
         prices_lite.append(i)
-        
+
 for i in items:
     if i not in items_lite:
         items_lite.append(i)
 
 for i in items_lite:
     print(*i[:len(i)-1],int(i[len(i)-1])*items.count(i))
+
+# https://www.hackerrank.com/challenges/compress-the-string
+
+s = input()
+count = 1
+result=[]
+i=0
+while(i<len(s)-2):
+    while(s[i]==s[i+1]):
+        count = count + 1
+        i=i+1
+    result.append((count,int(s[i])))
+    i=i+1
+    count=1
+if s[len(s)-2]==s[len(s)-1]:
+    result.append((2,int(s[len(s)-1])))
+else:
+    result.append((1,int(s[len(s)-1])))
+    result.append((1,int(s[len(s)-2])))
+print(*result)
